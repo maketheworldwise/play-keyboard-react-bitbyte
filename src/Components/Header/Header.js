a491900cb2b4e9b0f07491c923f9c848738d44c8
이고운 <kowoon@igoun-ui-MacBookAir.local>
2022-10-05 16:06:49 +0900

    Modify: 상단 아이콘 삭제 수정

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,22 +1,11 @@
 import styled from 'styled-components';
 import { BiSearch } from 'react-icons/bi';
 import logo from '../../Assets/logo.png';
-import Siganl from '../../Assets/Svgs/top_nav/signal.png';
-import Wifi from '../../Assets/Svgs/top_nav/wifi.png';
-import Battery from '../../Assets/Svgs/top_nav/battery.png';
 
 function Header() {
   return (
     <>
       <StyledHeader>
-        <div className="timeContainer">
-          <p className="time">9:41</p>
-          <div className="mobile_style">
-            <img src={Siganl} alt="icon" />
-            <img src={Wifi} alt="icon" />
-            <img src={Battery} alt="icon" />
-          </div>
-        </div>
         <div className="titleContainer">
           <div className="title">
             <img src={logo} alt="logo"></img>
@@ -39,42 +28,13 @@ const StyledHeader = styled.div`
   height: 123px;
   margin: 0 auto;
 
-  div.timeContainer {
-    display: flex;
-    justify-content: space-between;
-
-    p.time {
-      position: relative;
-      width: 54px;
-      height: 18px;
-      top: 14px;
-      left: 10px;
-      font-weight: 600;
-      font-size: 15px;
-      line-height: 18px;
-      text-align: center;
-    }
-
-    div.mobile_style {
-      display: inline-block;
-      position: relative;
-      top: 14px;
-      right: 10px;
-      margin-right: 10px;
-
-      img {
-        margin-left: 4px;
-      }
-    }
-  }
-
   div.titleContainer {
     display: flex;
     position: absolute;
     width: 100%;
     max-width: 720px;
     height: 56px;
-    top: 44px;
+    top: 22px;
     align-items: center;
 
     div.title {
@@ -99,14 +59,13 @@ const StyledHeader = styled.div`
       }
     }
   }
-
   div.h3box {
     display: flex;
     position: relative;
     width: 100%;
     height: 24px;
-    left: 16px;
     top: 80px;
+    left: 16px;
     margin-left: 10px;
 
     h3 {
