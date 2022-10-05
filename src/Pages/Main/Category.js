9eb23cb1ca99d4d9006cb34f8ffe116d2ef2db9c
Miog Yang <99234582+miogy@users.noreply.github.com>
2022-10-05 15:32:49 +0900

    Feature: 기능추가 (#12)
    UI추가, 기능추가, api연결
    
    Issue: #2

---
+++ src/Pages/Main/Category.js
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+function Category({ category, handleName }) {
+  const [active, setActive] = useState('');
+  return (
+    <>
+      <Wrap>
+        {category.map(list => {
+          return (
+            <li key={list}>
+              <span
+                className={active === list ? 'active' : ''}
+                onClick={e => {
+                  //console.log(e.target); //consol로 태그확인
+                  setActive(e.target.textContent);
+                  handleName(list);
+                }}
+              >
+                {list}
+              </span>
+            </li>
+          );
+        })}
+      </Wrap>
+    </>
+  );
+}
+export default Category;
+
+const Wrap = styled.ul`
+  width: 100%;
+  height: 48px;
+  display: flex;
+  overflow-x: scroll;
+  /* border: 1px solid blue; */
+  -ms-overflow-style: none;
+  li {
+    margin-right: 16px;
+    padding-top: 14px;
+    span {
+      margin-right: 12px;
+      margin-left: 12px;
+      width: 100%;
+      display: block;
+      text-align: center;
+      height: 32px;
+      color: #aaabb3;
+      &.active {
+        /* width: 100%; */
+        font-weight: 700;
+        color: #ff417d;
+        border-bottom: 2px solid #ff417d;
+        box-sizing: content-box;
+      }
+    }
+  }
+
+  ::-webkit-scrollbar {
+    display: none;
+    width: 0 !important;
+    scrollbar-width: none;
+  }
+`;
--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
 import { getCategories, getThemes } from './Api';
+import Category from './Category';
 
 import styles from './Main.module.scss';
 
 function Main() {
-  return <div className={styles.main_container}>Main</div>;
+  const [category, setCategory] = useState([]);
+  const [name, setName] = useState([]);
+
+  useEffect(() => {
+    getCategories().then(json => setCategory(json.data));
+    getThemes(name).then(
+      // json => console.log(json.data) //data consol로 확인
+      json => setName(json.data)
+    );
+  }, [name]);
+
+  const handleName = el => {
+    setName(el);
+  };
+
+  return (
+    <div className={styles.main_container}>
+      <Category handleName={handleName} category={category} />
+    </div>
+  );
 }
 
 export default Main;
