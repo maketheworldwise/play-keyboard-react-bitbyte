a58ce0f67606eb49bd0aa4f141b04ef081259652
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-10-05 14:14:02 +0900

    Feature: 테마 상세 뒤로가기 버튼 (#9)
    테마 상세 뒤로가기 버튼 구현
    
    Issue: #8

--- src/Components/DetailInfo/DetailInfo.js
+++ src/Components/DetailInfo/DetailInfo.js
@@ -1,14 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+
 import HashTag from './HashTag/HashTag';
 import HashTagContainer from './HashTag/HashTagContainer';
 
 import styles from './DetailInfo.module.scss';
+import IcAllBack from '../../Assets/Svgs/ic_all_back.svg';
 import IcAllShare from '../../Assets/Svgs/ic_all_share.svg';
 
 function DetailInfo(props) {
   const data = props.data;
+  const navigate = useNavigate();
+  const handleBackClick = () => {
+    navigate('/main');
+  };
 
   return (
     <div className={styles.detail_info_container}>
+      <div className={styles.detail_back_btn} onClick={handleBackClick}>
+        <img src={IcAllBack} alt="none" />
+      </div>
       <div className={styles.detail_image}>
         <img src={data.imageUrl} alt="none" />
       </div>
