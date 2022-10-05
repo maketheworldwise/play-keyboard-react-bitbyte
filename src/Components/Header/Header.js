1305fd6a1f950305a27080fd4a91e62ea36ccb9a
miogy <jjangrl87@gmail.com>
2022-10-05 14:50:54 +0900

    Feature:카테고리

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,7 @@
-import Category from './Category';
 import styles from './Header.module.scss';
 
 function Header() {
-  return (
-    <div className={styles.header_container}>
-      <Category />
-    </div>
-  );
+  return <div className={styles.header_container}></div>;
 }
 
 export default Header;
--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,9 +1,36 @@
+import { useEffect, useState } from 'react';
 import { getCategories, getThemes } from './Api';
+import Category from './Category';
 
 import styles from './Main.module.scss';
 
 function Main() {
-  return <div className={styles.main_container}>Main</div>;
+  const [category, setCategory] = useState([]);
+  const [filter, setFilter] = useState([]);
+  const [name, setName] = useState([]);
+
+  useEffect(() => {
+    getCategories().then(json => setCategory(json.data));
+    getThemes(name).then(
+      json => console.log(json.data)
+      // setName(json.data)
+    );
+  }, [name]);
+
+  const handleName = el => {
+    setName(el);
+  };
+
+  return (
+    <div className={styles.main_container}>
+      <Category
+        handleName={handleName}
+        category={category}
+        filter={filter}
+        setFilter={setFilter}
+      />
+    </div>
+  );
 }
 
 export default Main;
